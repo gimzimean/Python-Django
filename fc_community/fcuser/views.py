ac5d80b6ed23dbcee0f45cfd18dbcdca28f1fdfd
GZM <gimzimean@gmail.com>
2020-06-04 16:30:51 +0900

    form을 이용한 커스터마이징

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Fcuser
+from .forms import LoginForm
 
 # Create your views here.
 
@@ -22,26 +23,31 @@ def logout(request):
 
 
 def login(request):
-    if request.method == "GET":
-        return render(request, 'login.html')  # 이미 templates 폴더를 바라보고 있슴
-    elif request.method == "POST":
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+    form = LoginForm()
+    return render(request, 'login.html', {'form': form})
 
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야합니다.'
-        else:
-            fcuser = Fcuser.objects.get(username=username)
-            if check_password(password, fcuser.password):
-                # 비밀번호 일치 ,로그인 처리
-                # 세션
-                request.session['user'] = fcuser.id  # 딕셔너리 user 키에 id값을 넣음
-                # redirect
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호가 틀렸습니다.'
-        return render(request, 'login.html', res_data)
+
+# def login(request):
+#     if request.method == "GET":
+#         return render(request, 'login.html')  # 이미 templates 폴더를 바라보고 있슴
+#     elif request.method == "POST":
+#         username = request.POST.get('username', None)
+#         password = request.POST.get('password', None)
+
+#         res_data = {}
+#         if not (username and password):
+#             res_data['error'] = '모든 값을 입력해야합니다.'
+#         else:
+#             fcuser = Fcuser.objects.get(username=username)
+#             if check_password(password, fcuser.password):
+#                 # 비밀번호 일치 ,로그인 처리
+#                 # 세션
+#                 request.session['user'] = fcuser.id  # 딕셔너리 user 키에 id값을 넣음
+#                 # redirect
+#                 return redirect('/')
+#             else:
+#                 res_data['error'] = '비밀번호가 틀렸습니다.'
+#         return render(request, 'login.html', res_data)
 
 
 def register(request):
