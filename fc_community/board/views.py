e215f9d0865cc09bba2208bfbd0229a170152b94
GZM <gimzimean@gmail.com>
2020-06-05 20:01:11 +0900

    페이징

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from .models import Board
 from fcuser.models import Fcuser
 from .forms import BoardForm
+from django.core.paginator import Paginator
 # Create your views here.
 
 
@@ -17,6 +18,7 @@ def board_detail(request, pk):
 def board_write(request):
     if not(request.session.get('user')):
         return redirect('/fcuser/login/')
+
     if request.method == "POST":
         form = BoardForm(request.POST)
         if form.is_valid():
@@ -33,9 +35,14 @@ def board_write(request):
 
     else:
         form = BoardForm()
-    return render(request, 'board_write.html', {'form': form})
+        return render(request, 'board_write.html', {'form': form})
 
 
 def board_list(request):
-    boards = Board.objects.all().order_by("-id")
+    all_boards = Board.objects.all().order_by("-id")
+    page = int(request.GET.get('page', 1))  # 지금 페이지는 뭐?
+    paginator = Paginator(all_boards, 3)  # 몇개씩?
+
+    boards = paginator.get_page(page)  # 뿌리기
+
     return render(request, 'board_list.html', {'boards': boards})
