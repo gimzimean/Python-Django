2f867b157b64eeaf948519e4637182f683e836cf
GZM <gimzimean@gmail.com>
2020-06-12 14:54:06 +0900

    태그 생성or 이용

--- fc_community/board/forms.py
+++ fc_community/board/forms.py
@@ -7,3 +7,5 @@ class BoardForm(forms.Form):
         'required': "제목을 입력해주세요."})
     contents = forms.CharField(label="내용", widget=forms.Textarea, error_messages={
                                'required': "내용을 입력해주세요."})
+
+    tags = forms.CharField(required=False, label='태그')
